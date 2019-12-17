0483de5c0f39c2d697971f92ec089b2c36f8627e
kekeyu2266 <puchao260@gmail.com>
2019-12-17 23:03:18 +0800

    1217

--- 1217/module.py
+++ 1217/module.py
@@ -5,17 +5,17 @@ import test_module
 
 # 复习文件打开
 print(os.getcwd())  # 当前目录是文件所在的上一级目录？
-tt = open("1217\\tt.txt", mode="r", encoding="utf-8")
+tt = open("1217/tt.txt", mode="r", encoding="utf-8")
 print(tt.read())
 tt.close()
 
 
 # a模式打开文件，指针在文件结尾，无法使用read（）读取
-tt = open(".\\1217\\tt.txt", "a", encoding="utf-8")
+tt = open("./1217/tt.txt", "a", encoding="utf-8")
 tt.write("\n5.你这个垃圾!\n------")
 tt.close()
 
-tt = open(".\\1217\\tt.txt", mode="r", encoding="utf-8")
+tt = open("./1217/tt.txt", mode="r", encoding="utf-8")
 print(tt.read())
 tt.close()
 
