87eccd1202265775f71a36e40f1fcfc62cc1a763
kekeyu2266 <puchao260@gmail.com>
2019-12-17 23:14:13 +0800

    1217

--- 1217/module.py
+++ 1217/module.py
@@ -2,20 +2,23 @@ import os
 from sys import path, argv
 from test_module2 import haha2
 import test_module
+from pathlib import Path  # 解决Mac与Windows系统文件路径分隔符不一致的问题
 
 # 复习文件打开
 print(os.getcwd())  # 当前目录是文件所在的上一级目录？
-tt = open("1217/tt.txt", mode="r", encoding="utf-8")
+folder = Path("1217/") / "tt.txt"  # 解决Mac与Windows系统文件路径分隔符不一致的问题
+print(folder)
+tt = open(folder, mode="r", encoding="utf-8")
 print(tt.read())
 tt.close()
 
 
 # a模式打开文件，指针在文件结尾，无法使用read（）读取
-tt = open("./1217/tt.txt", "a", encoding="utf-8")
+tt = open(folder, "a", encoding="utf-8")
 tt.write("\n5.你这个垃圾!\n------")
 tt.close()
 
-tt = open("./1217/tt.txt", mode="r", encoding="utf-8")
+tt = open(folder, mode="r", encoding="utf-8")
 print(tt.read())
 tt.close()
 
